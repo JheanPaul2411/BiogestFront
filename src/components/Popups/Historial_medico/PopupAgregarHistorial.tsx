41cc6623805c41dd41c2f2a946f56adedc5b5c6d
Mathew288 <guachrichard@gmail.com>
2024-05-12 23:28:49 -0500

    refactor (historial medico): categorizacion de inputs en el registro d euna nueva incidencia en el historial medico

--- src/components/Popups/Historial_medico/PopupAgregarHistorial.tsx
+++ src/components/Popups/Historial_medico/PopupAgregarHistorial.tsx
@@ -5,6 +5,7 @@ import { Usuario } from "@/helpers/models/User";
 import { Modal, Label, TextInput, Textarea, Button } from "flowbite-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { Accordion } from "flowbite-react";
 
 interface PropsPopupEditarCita {
   selectedUser: Usuario;
@@ -51,7 +52,7 @@ const PopupAgregarHistorial: React.FC<PropsPopupEditarCita> = ({
   return (
     <Modal
       onClose={onClose}
-      size={"5xl"}
+      size={"2xl"}
       className="z-[60]"
       popup
       show={true}
@@ -59,141 +60,159 @@ const PopupAgregarHistorial: React.FC<PropsPopupEditarCita> = ({
     >
       <Modal.Header />
       <Modal.Body>
-        <form className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="peso" className="mb-2">
-              Peso (Kg):
-            </Label>
-            <TextInput
-              type="number"
-              id="peso"
-              name="peso"
-              value={historialMedico.peso || ""}
-              onChange={handleChange}
-            />
-          </div>
+        <form>
+          <Accordion>
+            <Accordion.Panel>
+              <Accordion.Title>Mediciones físicas</Accordion.Title>
+              <Accordion.Content>
+                <div className="flex flex-col">
+                  <Label htmlFor="peso" className="mb-2">
+                    Peso (Kg):
+                  </Label>
+                  <TextInput
+                    type="number"
+                    id="peso"
+                    name="peso"
+                    value={historialMedico.peso || ""}
+                    onChange={handleChange}
+                  />
+                </div>
 
-          <div className="flex flex-col mb-4">
-            <Label htmlFor="presionArterial" className="mb-2">
-              Presión Arterial:
-            </Label>
-            <TextInput
-              type="text"
-              id="presionArterial"
-              name="presionArterial"
-              value={historialMedico.presionArterial}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="temperatura" className="mb-2">
-              Temperatura:
-            </Label>
-            <TextInput
-              type="number"
-              id="temperatura"
-              name="temperatura"
-              value={historialMedico.temperatura || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="enfermedades" className="mb-2">
-              Enfermedades:
-            </Label>
-            <TextInput
-              type="text"
-              id="enfermedades"
-              name="enfermedades"
-              value={historialMedico.enfermedades}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4">
-            <Label htmlFor="alergias" className="mb-2">
-              Alergias:
-            </Label>
-            <TextInput
-              type="text"
-              id="alergias"
-              name="alergias"
-              value={historialMedico.alergias}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="medicamentos" className="mb-2">
-              Medicamentos:
-            </Label>
-            <TextInput
-              type="text"
-              id="medicamentos"
-              name="medicamentos"
-              value={historialMedico.medicamentos}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="antecedentesFamiliares" className="mb-2">
-              Antecedentes Familiares:
-            </Label>
-            <Textarea
-              id="antecedentesFamiliares"
-              name="antecedentesFamiliares"
-              value={historialMedico.antecedentesFamiliares}
-              onChange={handleChange}
-            />
-          </div>
-      
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="observaciones" className="mb-2">
-              Observaciones:
-            </Label>
-            <Textarea
-              id="observaciones"
-              name="observaciones"
-              value={historialMedico.observaciones}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="diagnostico" className="mb-2">
-              Diagnóstico:
-            </Label>
-            <Textarea
-              id="diagnostico"
-              name="diagnostico"
-              value={historialMedico.diagnostico}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4">
-            <Label htmlFor="tratamiento" className="mb-2">
-              Tratamiento:
-            </Label>
-            <Textarea
-              id="tratamiento"
-              name="tratamiento"
-              value={historialMedico.tratamiento}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col mb-4 md:mr-4">
-            <Label htmlFor="proximaCita" className="mb-2">
-              Fecha de la cita:
-            </Label>
-            <TextInput
-              type="date"
-              id="proximaCita"
-              name="proximaCita"
-              value={fechaConsulta}
-              onChange={(e) => setFechaConsulta(e.target.value)}
-            />
-          </div>
+                <div className="flex flex-col">
+                  <Label htmlFor="presionArterial" className="mb-2">
+                    Presión Arterial:
+                  </Label>
+                  <TextInput
+                    type="text"
+                    id="presionArterial"
+                    name="presionArterial"
+                    value={historialMedico.presionArterial}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <Label htmlFor="temperatura" className="mb-2">
+                    Temperatura:
+                  </Label>
+                  <TextInput
+                    type="number"
+                    id="temperatura"
+                    name="temperatura"
+                    value={historialMedico.temperatura || ""}
+                    onChange={handleChange}
+                  />
+                </div>
+              </Accordion.Content>
+            </Accordion.Panel>
 
-          <div className="lg:col-span-2">
+            <Accordion.Panel>
+              <Accordion.Title>Historial médico</Accordion.Title>
+              <Accordion.Content>
+                <div className="flex flex-col mb-4 md:mr-4">
+                  <Label htmlFor="enfermedades" className="mb-2">
+                    Enfermedades:
+                  </Label>
+                  <TextInput
+                    type="text"
+                    id="enfermedades"
+                    name="enfermedades"
+                    value={historialMedico.enfermedades}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col mb-4">
+                  <Label htmlFor="alergias" className="mb-2">
+                    Alergias:
+                  </Label>
+                  <TextInput
+                    type="text"
+                    id="alergias"
+                    name="alergias"
+                    value={historialMedico.alergias}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col mb-4 md:mr-4">
+                  <Label htmlFor="medicamentos" className="mb-2">
+                    Medicamentos:
+                  </Label>
+                  <TextInput
+                    type="text"
+                    id="medicamentos"
+                    name="medicamentos"
+                    value={historialMedico.medicamentos}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col mb-4 md:mr-4">
+                  <Label htmlFor="antecedentesFamiliares" className="mb-2">
+                    Antecedentes Familiares:
+                  </Label>
+                  <Textarea
+                    id="antecedentesFamiliares"
+                    name="antecedentesFamiliares"
+                    value={historialMedico.antecedentesFamiliares}
+                    onChange={handleChange}
+                  />
+                </div>
+              </Accordion.Content>
+            </Accordion.Panel>
+
+            <Accordion.Panel>
+              <Accordion.Title>Relacionados a la consulta</Accordion.Title>
+              <Accordion.Content>
+                <div className="flex flex-col mb-4 md:mr-4">
+                  <Label htmlFor="observaciones" className="mb-2">
+                    Observaciones:
+                  </Label>
+                  <Textarea
+                    id="observaciones"
+                    name="observaciones"
+                    value={historialMedico.observaciones}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col mb-4 md:mr-4">
+                  <Label htmlFor="diagnostico" className="mb-2">
+                    Diagnóstico:
+                  </Label>
+                  <Textarea
+                    id="diagnostico"
+                    name="diagnostico"
+                    value={historialMedico.diagnostico}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col mb-4">
+                  <Label htmlFor="tratamiento" className="mb-2">
+                    Tratamiento:
+                  </Label>
+                  <Textarea
+                    id="tratamiento"
+                    name="tratamiento"
+                    value={historialMedico.tratamiento}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col mb-4 md:mr-4">
+                  <Label htmlFor="proximaCita" className="mb-2">
+                    Fecha de la cita:
+                  </Label>
+                  <TextInput
+                    type="date"
+                    id="proximaCita"
+                    name="proximaCita"
+                    value={fechaConsulta}
+                    onChange={(e) => setFechaConsulta(e.target.value)}
+                  />
+                </div>
+              </Accordion.Content>
+            </Accordion.Panel>
+          </Accordion>
+
+          <div className="lg:col-span-2 my-5">
             <Button color="purple" onClick={handleAgregarCita}>
-              Agregar ficha médica
+              Agregar incidencia
             </Button>
           </div>
         </form>
